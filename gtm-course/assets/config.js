0f388e00c5814d747bdd07184195e5034d8ab4c7
Sigrid Konradsson <sigrid.konradsson@gmail.com>
2026-09-07 11:13:00 +0200

    Update GTM cointainer

--- gtm-course/assets/config.js
+++ gtm-course/assets/config.js
@@ -13,7 +13,7 @@
 
 window.GTM_COURSE_CONFIG = {
 
-    containerId: '',
+    containerId: 'GTM-TNV7J86H',
 
     /* Optional. Set to true to hide the "paste your container ID" box on every
        page, once you have set the ID above and do not want students changing it. */
